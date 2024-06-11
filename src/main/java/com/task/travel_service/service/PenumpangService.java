460c157e0bf6f36297df99bb9790dc59c615f909
DESKTOP-8CB4EQR\USER <seno.priyambodo13@gmail.com>
2024-06-11 20:58:12 +0700

    remove unused import

--- src/main/java/com/task/travel_service/service/PenumpangService.java
+++ src/main/java/com/task/travel_service/service/PenumpangService.java
@@ -5,7 +5,6 @@ import com.task.travel_service.entity.PenumpangEntity;
 import com.task.travel_service.exception.DataNotFoundException;
 import com.task.travel_service.repository.PenumpangRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
