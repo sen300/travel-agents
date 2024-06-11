015df27d09d1afa8f08c37b8329033b0b7d430c7
DESKTOP-8CB4EQR\USER <seno.priyambodo13@gmail.com>
2024-06-11 20:56:13 +0700

    update exception method

--- src/main/java/com/task/travel_service/exception/CustomExceptionHandler.java
+++ src/main/java/com/task/travel_service/exception/CustomExceptionHandler.java
@@ -2,19 +2,20 @@ package com.task.travel_service.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
-@ControllerAdvice
-public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(value = {ApiRequestException.class})
-    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
-        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> handleBadRequestException(MethodArgumentNotValidException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 e,
@@ -22,8 +23,20 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
                 ZonedDateTime.now(ZoneId.of("Asia/Jakarta"))
         );
 
-        return new ResponseEntity<>(apiException, badRequest);
+        return new ResponseEntity<>(apiException, status);
     }
 
-    // Other exception handlers for different scenarios
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = {DataNotFoundException.class})
+    public ResponseEntity<Object> handleApiRequestException(DataNotFoundException e) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                e,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ZonedDateTime.now(ZoneId.of("Asia/Jakarta"))
+        );
+
+        return new ResponseEntity<>(apiException, status);
+    }
 }
